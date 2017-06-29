496dfdf01504fe3f3364e6c70c83d00855157936
e5ash <e5ash.bro@gmail.com>
2017-06-30 00:56:55 +0300

    Uploading files

--- app/template/scripts/scripts.js
+++ app/template/scripts/scripts.js
@@ -1,15 +1,5 @@
 $(document).ready(function($) {
-	$('form').submit(function() {
-		$.ajax({
-		    type: "POST",
-		    url: "/order.php",
-		    data: $(this).serialize()
-		}).done(function() {
-		    popupForm.css('display','none');
-		    popupMessage.css('display','block');
-		});
-		return false;
-	});
+	$('.trusted__list').slick();
 });
 
 
